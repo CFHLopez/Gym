9cfa23bacd314393e27556f9a546b0626cff477a
Christian Lopez Chavez <Christian.Lopez@alumnos.uda.cl>
2017-06-21 13:18:39 -0400

    volviendo a solucionar error vista alumnos

--- app/controllers/alumnos_controller.rb
+++ app/controllers/alumnos_controller.rb
@@ -50,6 +50,6 @@ class AlumnosController < ApplicationController
   end
 
   def alumno_params
-    params.require(:alumno).permit(:nombre, :sexo, :plan_id)
+    params.require(:alumno).permit(:nombre, :sexo, :plan_id, :create_at)
   end
 end
