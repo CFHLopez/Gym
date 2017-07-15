2f5dded2597edb66fd37512939591b361001a6ee
Katherinne saez <kangel_ksb@hotmail.com>
2017-07-15 19:55:37 -0400

    arreglando problema con devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,8 @@ before_filter :configure_permitted_parameters, if: :devise_controller?
 
 protected
   def configure_permitted_parameters        
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:nombre,:rut, :edad, :sexo , :role, :observacion, :email, :password) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:nombre,:rut, :edad, :sexo , :role, :observacion, :email, :password, :current_password, :is_female, :date_of_birth) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:nombre,:rut, :edad, :sexo , :role, :observacion, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:nombre,:rut, :edad, :sexo , :role, :observacion, :email, :password, :current_password, :is_female, :date_of_birth) }
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -113,19 +113,19 @@ ActiveRecord::Schema.define(version: 20170715005629) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",         null: false
+    t.string   "encrypted_password",     default: "",         null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,          null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "role"
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
+    t.string   "role",                   default: "profesor"
     t.string   "nombre"
     t.string   "rut"
     t.integer  "edad"
@@ -144,6 +144,16 @@ ActiveRecord::Schema.define(version: 20170715005629) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.integer  "clase_id"
+  end
+
+  create_table "usuarios", force: :cascade do |t|
+    t.string   "nombre"
+    t.string   "email"
+    t.string   "contraseña"
+    t.string   "estado"
+    t.integer  "tipo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "administradors", "clases"
