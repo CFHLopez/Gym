90268fa949bf77fcf0872a1fd3f4cc1d85c54a77
Christian F. H. Lopez Chavez <christian.lopez@alumnos.uda.cl>
2017-07-26 12:16:56 -0400

    modificacion al form de fichas

--- app/controllers/usuario_controller.rb
+++ app/controllers/usuario_controller.rb
@@ -1,6 +1,5 @@
 class UsuarioController < ApplicationController
   def index
-  	     @users = User.paginate(:page => params[:page], :per_page => 5)
+  	@users = User.paginate(:page => params[:page], :per_page => 5)
   end
-
 end
