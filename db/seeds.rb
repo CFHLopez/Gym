f8f29fb770d5818fbb61538ada9ab67d956de2cd
Christian F. H. Lopez Chavez <christian.lopez@alumnos.uda.cl>
2017-07-26 14:11:55 -0400

    seeds de user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = User.new
+users.email = 'funcionalitychris@gmail.com'
+users.encrypted_password = '12345678'
+reset_password_token = '12345678'
+users.role = 'Administrador'
+users.nombre = 'Christian Lopez'
+users.edad = '27'
+users.sexo = 'Masculino'
+users.rut = '17386250-4'
+users.observacion = 'Estudiante Ing. Software'
+users.save!
