b4e3a6cd6337a20d91384c646482fcca8fb77db8
Christian F. H. Lopez Chavez <christian.lopezc@utem.cl>
2017-11-17 11:31:40 -0300

    modificacion a la funcion morosos, cambio de n.alumno_id por n.rut debido al cambio en la forma de pago

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+#usuarios
+
 users = User.new
 users.email = 'christian.lopez@alumnos.uda.cl'
 users.encrypted_password = 'dx15071990'
@@ -19,3 +21,16 @@ users.sexo = 'Masculino'
 users.rut = '17386250-4'
 users.observacion = 'Estudiante Ing. Software'
 users.save
+
+#planes
+pl1 = Plan.new
+pl1.nombre = '3xS'
+pl1.precio = '15000'
+pl1.desc = 'Asistencia 3 veces por semana'
+pl1.save
+
+pl2 = Plan.new
+pl2.nombre = 'Libre'
+pl2.precio = '18000'
+pl2.desc = 'Asistencia lunes a sabado'
+pl2.save
